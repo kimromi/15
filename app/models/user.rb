c6a11447cb6ad9071acdebd5a17b9ed2b24664bf
kimromi <kimromi4@gmail.com>
2017-09-28 02:13:17 +0900

    implements twitter callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:twitter]
+
+  def self.from_omniauth(oauth)
+    find_or_create_by(provider: oauth['provider'], uid: oauth['uid']) do |user|
+      user.provider = oauth['provider']
+      user.uid = oauth['uid']
+      user.username = oauth['info']['nickname']
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'top#index'
   get 'top/index'
 
-  devise_for :users
+  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
 end
