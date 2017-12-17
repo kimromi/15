f8bae5d6d9581d229add83de591745d596678591
kimromi <kimromi4@gmail.com>
2017-12-18 02:30:29 +0900

    fix invitation bug

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,17 +6,18 @@ class InvitationsController < ApplicationController
       return
     end
 
-    if signed_in? && @invitation.team.members.pluck(:user_id).include?(current_user.id)
-      redirect_to "/#{@invitation.team.name}", alert: "Already join #{@invitation.team.name} team."
-    end
+    redirect_to_app_if_already_joined(@invitation)
 
     session[:invitation_id] = @invitation.id
     @team = @invitation.team
   end
 
   def update
+    binding.pry
     @invitation = Invitation.find_by_token(invitation_params[:token])
-    redirect_to root_path, notice: "Join #{@invitation.team.name} team!!"
+    redirect_to_app_if_already_joined(@invitation)
+    @invitation.team.members.create(user_id: current_user.id)
+
     redirect_to "/#{@invitation.team.name}", notice: "Join #{@invitation.team.name} team!!"
   end
 
@@ -24,5 +25,11 @@ class InvitationsController < ApplicationController
 
   def invitation_params
     params.require(:invitation).permit(:token)
+  end
+
+  def redirect_to_app_if_already_joined(invitation)
+    if signed_in? && invitation.team.members.pluck(:user_id).include?(current_user.id)
+      redirect_to "/#{invitation.team.name}", alert: "Already join #{invitation.team.name} team."
+    end
   end
 end
