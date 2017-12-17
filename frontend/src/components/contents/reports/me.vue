e23a549b4227078920813ecb008dc27e45fe621a
kimromi <kimromi4@gmail.com>
2017-12-17 15:23:35 +0900

    add report page

---
+++ frontend/src/components/contents/reports/me.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="reports">
+    <h1>Me</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+      };
+    },
+    created: async function() {
+    },
+    methods: {
+    }
+  };
+</script>
+
+<style lang="scss">
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,6 +4,10 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 import App from './components/app.vue';
+import Reports from './components/contents/reports.vue';
+import MyReports from './components/contents/reports/me.vue';
+import TeamReports from './components/contents/reports/team.vue';
+
 import Tasks from './components/contents/tasks.vue';
 import Team from './components/contents/team.vue';
 import Me from './components/contents/me.vue';
@@ -17,6 +21,11 @@ const router = new VueRouter({
   routes: [
     {
       path: '/', component: App, children: [
+        { path: 'reports', component: Reports, children: [
+          { path: '', redirect: 'me' },
+          { path: 'me', component: MyReports, name: 'my reports' },
+          { path: 'team', component: TeamReports, name: 'team reports' },
+        ]},
         { path: 'tasks', component: Tasks },
         { path: 'team', component: Team },
         { path: 'members/me', component: Me },
